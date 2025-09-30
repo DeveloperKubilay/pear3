1a1dc6dcfae95ead68490bd85de58468acb50038
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-09-30 18:29:49 +0300

    fix: güncellenmiş modül yolu ve versiyon numarası düzeltildi

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const Pear = require('./module/index.js');
+const Pear = require("./module");
 
 async function main() {
-    const browser = await Pear({
-        viewport: { width: 1280, height: 800 },
-        headless: false
-    });
+  const browser = await Pear({
+    viewport: { width: 1280, height: 800 },
+    headless: false
+  });
 
-    const page = await browser.newPage();
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+  await new Promise(r => setTimeout(r, 2000)); // Wait for 5 seconds
+  const html = await page.content();
+  console.log(html);
 
-    await page.goto("https://google.com");
-    await page.directType('textarea', 'Hello Pear!');
-    await page.keypress('Enter');
+  const screenshot = await page.screenshot();
+  console.log(screenshot);
 
-
-
+  await browser.close();
 }
 
-main()
+main().catch(console.error);
